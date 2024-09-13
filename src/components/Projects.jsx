462f5ecef20fa03297a929f9969845fa23a2fcf9
Albert Jiang <galar@wireless-10-148-87-151.public.utexas.edu>
2024-09-13 15:56:24 -0500

    Add chess game

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,47 +17,41 @@ export default function Projects(){
 
             <TableOfContents/>
 
-            <Box sx={{mt: 5, pl: '15%', pr: '15%', pt: 5, pb: 5}}>
-                <ProjectEntry
-                projectTitle='Anime Showdown'
-                projectDate='August 2024'
-                tags={['React', 'Express', 'Databases', 'Game']}
-                caption='A single-player, progression based card game.'
-                descriptions={['Anime Showdown is a card game featuring nearly a dozen of the most iconic anime characters each with unique in-game abilities. Fight alongside and against your favorite characters, create a synergistic team, and level up your cards to fight stronger bosses as you progress through levels.',
-                    "This was my first time using framer motion, and my experience with it has been pleasantly surprising; although I didn't delve too deep into its capabilities, I was able to create clean, animated transitions relatively painlessly.", 
-                    "I plan to revisit this project sometime in the far future and add a multiplayer functionality (plus other cool features)."
-                ]}
-                imgSrc={'anime_showdown.png'}
-                linkNames={['Github', 'Website']}
-                linkUrls={['https://github.com/beastgwert/anime-showdown', 'https://anime-showdown.vercel.app/']}
-                />
-            </Box>
-            <Box sx={{mt: 5, pl: '15%', pr: '15%', pt: 5, pb: 5}}>
-                <ProjectEntry
-                projectTitle='A2C Resume Builder'
-                projectDate='June 2024'
-                tags={['React', 'Tailwind']}
-                caption='Highly customizable resume builder for college apps.'
-                descriptions={["Applying for college is hard enough and there's no reason to make it harder than necessary. This app is designed to help high schoolers build a personalized resume, or at the very least, get an idea of how their current activities translate into a professional setting",
-                     "This project was my gateway into React, and transitioning from vanilla JS was quite a learning curve—especially considering the amount of interactivity required."]}
-                imgSrc={'a2c_resume_builder.png'}
-                linkNames={['Github', 'Website']}
-                linkUrls={['https://github.com/beastgwert/a2c-resume-builder', 'https://a2c-resume-builder.vercel.app/']}
-                />
-            </Box>
-            <Box sx={{mt: 5, pl: '15%', pr: '15%', pt: 5, pb: 5}}>
-                <ProjectEntry
-                projectTitle='Command Line Chess'
-                projectDate='June 2023'
-                tags={['Ruby', 'OOP']}
-                caption='Interactive chess from the terminal against an unintelligent bot.'
-                descriptions={["My description",
-                     "This project was my gateway into React, and transitioning from vanilla JS was quite a learning curve—especially considering the amount of interactivity required."]}
-                imgSrc={'command_line_chess.png'}
-                linkNames={['Github']}
-                linkUrls={['https://github.com/beastgwert/chess-game']}
-                />
-            </Box>
+            <ProjectEntry
+            projectTitle='Anime Showdown'
+            projectDate='August 2024'
+            tags={['React', 'Express', 'Databases', 'Game']}
+            caption='A single-player, progression based card game.'
+            descriptions={['Anime Showdown is a card game featuring nearly a dozen of the most iconic anime characters each with unique in-game abilities. Fight alongside and against your favorite characters, create a synergistic team, and level up your cards to fight stronger bosses as you progress through levels.',
+                "This was my first time using framer motion, and my experience with it has been pleasantly surprising; although I didn't delve too deep into its capabilities, I was able to create clean, animated transitions relatively painlessly.", 
+                "I plan to revisit this project sometime in the far future and add a multiplayer functionality (plus other cool features)."
+            ]}
+            imgSrc={'anime_showdown.png'}
+            linkNames={['Github', 'Website']}
+            linkUrls={['https://github.com/beastgwert/anime-showdown', 'https://anime-showdown.vercel.app/']}
+            />
+            <ProjectEntry
+            projectTitle='A2C Resume Builder'
+            projectDate='June 2024'
+            tags={['React', 'Tailwind']}
+            caption='Highly customizable resume builder for college apps.'
+            descriptions={["Applying for college is hard enough and there's no reason to make it more difficult than necessary. This app is designed to help high schoolers build a personalized resume, or at the very least, learn how their current activities translate into a professional setting",
+                    "This project was my gateway into React, and transitioning from vanilla JS was quite a learning curve—especially considering the amount of interactivity required in a resume builder."]}
+            imgSrc={'a2c_resume_builder.png'}
+            linkNames={['Github', 'Website']}
+            linkUrls={['https://github.com/beastgwert/a2c-resume-builder', 'https://a2c-resume-builder.vercel.app/']}
+            />
+            <ProjectEntry
+            projectTitle='Command Line Chess'
+            projectDate='June 2023'
+            tags={['Ruby', 'OOP']}
+            caption='Interactive chess from the terminal against an unintelligent bot.'
+            descriptions={["Unfortunately, replit terminated their hosting services so in order to play the game you need to run the commands in the repo. The interface is pretty primitive by nature of the command line and pieces may vary in size depending on your specs. It should be nearly impossible to lose since the bot makes random moves, but let me know if you do! ",
+                    "I started this project to practice object oriented programming and I may attempt to make my own chess AI in the future (ideally with a prettier interface) using Alpha-Beta pruning."]}
+            imgSrc={'command_line_chess.png'}
+            linkNames={['Github']}
+            linkUrls={['https://github.com/beastgwert/chess-game']}
+            />
         </Box>
     )
 }
